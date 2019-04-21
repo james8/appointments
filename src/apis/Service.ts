6f2f9337f911b890eded3a850da89ca8cc949bf4
unknown <james8@hawaii.edu>
2019-04-20 19:43:22 -1000

    Updated test data with more real data

--- src/apis/Service.ts
+++ src/apis/Service.ts
@@ -16,8 +16,16 @@ export class Service {
     public static GetServices(): Promise<Array<Service>> {
         return new Promise((resolve, reject) => {
             const data: Array<Service> = [
-                new Service(0, "Service 1", "Serivce 1 description", 1),
-                new Service(1, "Service 2", "Serivce 2 description", 2),
+                new Service(0, "30min+", "extra 30min", 2),
+                new Service(1, "Callus Removal", "Callus Removal", 0),
+                new Service(2, "Design", "hands or toes", 2),
+                new Service(3, "Ext", "extensions full set", 2),
+                new Service(4, "Fix", "fix", 2),
+                new Service(5, "Gel", "gel hand", 4),
+                new Service(6, "Gel Pedi", "gel toe + callus (no massage)", 4),
+                new Service(7, "Gel Toe", "gel toe (no callus/massage)", 4),
+                new Service(8, "Mani", "with polish", 4),
+                new Service(9, "Refill Toe", "refill toe", 1),
             ];
             resolve(data);
         });
